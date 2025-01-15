69ac43ba7cbcf51417f345f65abc2065c3801cc2
blepping <blepping@tuta.io>
2025-01-15 03:05:45 -0700

    Revert change to first_block_cache.py

--- first_block_cache.py
+++ first_block_cache.py
@@ -386,10 +386,12 @@ def create_patch_unet_model__forward(model,
     from comfy.ldm.modules.diffusionmodules.openaimodel import timestep_embedding, forward_timestep_embed, apply_control
 
     def call_remaining_blocks(self, transformer_options, control,
-                              transformer_patches, hs, h, *args, start_block=2, **kwargs):
+                              transformer_patches, hs, h, *args, **kwargs):
         original_hidden_states = h
 
-        for id, module in enumerate(self.input_blocks[start_block:], start=start_block):
+        for id, module in enumerate(self.input_blocks):
+            if id < 2:
+                continue
             transformer_options["block"] = ("input", id)
             h = forward_timestep_embed(module, h, *args, **kwargs)
             h = apply_control(h, control, 'input')
@@ -476,7 +478,9 @@ def create_patch_unet_model__forward(model,
         can_use_cache = False
 
         h = x
-        for id, module in enumerate(self.input_blocks[:2]):
+        for id, module in enumerate(self.input_blocks):
+            if id >= 2:
+                break
             transformer_options["block"] = ("input", id)
             if id == 1:
                 original_h = h
